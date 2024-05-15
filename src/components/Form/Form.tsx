f76db5b6d3c44032005d522504e4f597b59a0767
dmitinev <mitinov.dmitriy@gmail.com>
2024-05-15 21:10:53 +0200

    Update styling and add validation to Form component
    Transformed the pixel values into rem units for the Form styles to enhance scalability and responsiveness. Additionally, incorporated validations via the 'Yup' library, ensuring input entered follows the correct url format. Added error-handling CSS classes to highlight incorrect user input.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,31 @@
+import cn from 'classnames';
 import { useFormik } from 'formik';
 import { ShortUrlFormValues } from 'src/types/types';
+import * as yup from 'yup';
 import { Button } from '../Button';
 import './Form.scss';
+
+const validationSchema: yup.ObjectSchema<ShortUrlFormValues> = yup
+  .object()
+  .shape({
+    url: yup.string().url('Please add a link').required(),
+  });
 
 export const Form = () => {
   const formik = useFormik<ShortUrlFormValues>({
     initialValues: {
       url: '',
     },
+    validationSchema: validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+
+  const classes = cn('shortUrlForm__inputField', {
+    form_error: formik.errors.url && formik.touched.url,
+  });
+
   return (
     <section className="urlFormSection">
       <div className="container">
@@ -24,11 +38,16 @@ export const Form = () => {
             type="text"
             name="url"
             placeholder="Shorten a link here..."
-            className="shortUrlForm__inputField"
+            className={classes}
             value={formik.values.url}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
           />
+          {formik.errors.url && formik.touched.url && (
+            <span className="shortUrlForm__inputFieldError">
+              Please add a link
+            </span>
+          )}
           <Button size="medium" type="submit" variant="square">
             Shorten It!
           </Button>
