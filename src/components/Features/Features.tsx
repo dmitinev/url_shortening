214615dd44d73dafab457e84884d7040347ca2f2
Dmitry Mitinev <mitinov.dmitriy@gmail.com>
2024-05-22 19:55:17 +0200

    refactor: Update component names, fix content spacing, and improve animation

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,25 +1,89 @@
-import { FeatureItem } from 'components/Features/FeatureItem.tsx';
+import { MFeatureItem } from 'components/Features/FeatureItem.tsx';
+import { motion } from 'framer-motion';
 import { pageFeatures } from '../../Config.ts';
 import './Features.scss';
 
+const textAnimation = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.15 },
+  }),
+};
+
+const textAnimationHorizontal = {
+  hidden: {
+    x: 100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2 },
+  }),
+};
+
+const featureAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2 },
+  }),
+};
+
 export const Features = () => {
   return (
-    <main className="features">
+    <motion.main
+      className="features"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.2 }}
+    >
       <div className="container features__container">
-        <header className="features__header">Advanced Statistics</header>
-        <p className="features__text">
+        <motion.header
+          className="features__header"
+          custom={1}
+          variants={textAnimation}
+        >
+          Advanced Statistics
+        </motion.header>
+        <motion.p
+          className="features__text"
+          custom={2}
+          variants={textAnimationHorizontal}
+        >
           Track how your links are performing across the web with our advanced
           statistics dashboard.
-        </p>
-        <div className="features__items">
+        </motion.p>
+        <motion.div
+          className="features__items"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.2 }}
+        >
           {pageFeatures.length > 0 &&
-            pageFeatures.map(({ body, title, id, icon }) => {
+            pageFeatures.map(({ body, title, id, icon }, index) => {
               return (
-                <FeatureItem key={id} title={title} body={body} icon={icon} />
+                <MFeatureItem
+                  custom={index + 1}
+                  variants={featureAnimation}
+                  key={id}
+                  title={title}
+                  body={body}
+                  icon={icon}
+                />
               );
             })}
-        </div>
+        </motion.div>
       </div>
-    </main>
+    </motion.main>
   );
 };
--- src/components/ShortenedLinks/ShortenedLinks.tsx
+++ src/components/ShortenedLinks/ShortenedLinks.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { loginSelector } from 'src/store/loginSlice/loginSelectors';
 import { useAppDispatch, useAppSelector } from 'src/store/store-hooks';
@@ -5,6 +6,30 @@ import { removeLinks } from 'src/store/urlSlice/UrlSlice';
 import { urlsSelector } from 'src/store/urlSlice/urlSelector';
 import { Button } from '../Button';
 import './ShortenedLinks.scss';
+
+const cardAnimation = {
+  initial: {
+    opacity: 0,
+    x: 100,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.5 },
+  },
+};
+
+const cardAnimationReversed = {
+  initial: {
+    opacity: 0,
+    x: -100,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.5 },
+  },
+};
 
 export const ShortenedLinks = () => {
   const { urls } = useAppSelector(urlsSelector);
@@ -28,27 +53,41 @@ export const ShortenedLinks = () => {
     <section className="shortenedLinks">
       <div className="container shortenedLinks__container">
         <div className="shortenedLinks__list">
-          {urls.map((url) => (
-            <div key={url.shortUrl} className="shortenedLinks__item">
-              <span className="shortenedLinks__item-link">
-                {url.originalUrl}
-              </span>
-              <span className="shortenedLinks__item-shortened">
-                {url.shortUrl}
-              </span>
-              <Button
-                onClick={() => copyClipboardHandler(url.shortUrl)}
-                style={
-                  copiedlinks.includes(url.shortUrl)
-                    ? { background: '#3b3054' }
-                    : undefined
+          {urls.map((url, index) => (
+            <AnimatePresence key={url.shortUrl}>
+              <motion.div
+                className="shortenedLinks__item"
+                initial={
+                  (index + 1) % 2 === 0
+                    ? cardAnimation.initial
+                    : cardAnimationReversed.initial
                 }
-                variant="square"
-                size="medium"
+                animate={
+                  (index + 1) % 2 === 0
+                    ? cardAnimation.animate
+                    : cardAnimationReversed.animate
+                }
               >
-                {copiedlinks.includes(url.shortUrl) ? 'Copied!' : 'Copy'}
-              </Button>
-            </div>
+                <span className="shortenedLinks__item-link">
+                  {url.originalUrl}
+                </span>
+                <span className="shortenedLinks__item-shortened">
+                  {url.shortUrl}
+                </span>
+                <Button
+                  onClick={() => copyClipboardHandler(url.shortUrl)}
+                  style={
+                    copiedlinks.includes(url.shortUrl)
+                      ? { background: '#3b3054' }
+                      : undefined
+                  }
+                  variant="square"
+                  size="medium"
+                >
+                  {copiedlinks.includes(url.shortUrl) ? 'Copied!' : 'Copy'}
+                </Button>
+              </motion.div>
+            </AnimatePresence>
           ))}
         </div>
       </div>
